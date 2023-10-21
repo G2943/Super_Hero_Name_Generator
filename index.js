144b94aa44bb2b0593baebd868ab4dd56cff2fe8
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-10-21 15:24:20 +0530

    genarate super hero name using npm ESM module

---
+++ index.js
@@ -0,0 +1,12 @@
+// var generateName = require('sillyname');
+
+// var sillyName = generateName();
+
+// console.log(`Name is ${sillyName}.`);
+
+
+import superheroes from 'superheroes';
+
+let superName = superheroes.random();
+
+console.log(`I am ${superName}!`);
